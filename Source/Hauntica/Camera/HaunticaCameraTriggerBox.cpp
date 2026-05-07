15fda5b05d37a051b0a73ae45983d0e3496962d5
Kamil Fiącek <fiacek.kamil@gmail.com>
2026-05-07 11:40:46 +0200

    feat(camera): add trigger visualization for editor builds

--- Source/Hauntica/Camera/HaunticaCameraTriggerBox.cpp
+++ Source/Hauntica/Camera/HaunticaCameraTriggerBox.cpp
@@ -5,23 +5,39 @@
 
 #include "Camera/CameraActor.h"
 #include "Components/ArrowComponent.h"
+#if WITH_EDITORONLY_DATA
+#include "Components/BillboardComponent.h"
+#endif
 #include "Components/ShapeComponent.h"
 #include "Kismet/GameplayStatics.h"
 
+#if WITH_EDITOR
+static TAutoConsoleVariable<bool> CVarShowCameraTriggers(
+	TEXT("Hauntica.ShowCameraTriggers"),
+	false,
+	TEXT("Show camera trigger boxes."),
+	ECVF_Cheat);
+#endif
 
 AHaunticaCameraTriggerBox::AHaunticaCameraTriggerBox()
 {
 	PrimaryActorTick.bCanEverTick = true;
 	PrimaryActorTick.bStartWithTickEnabled = false;
 	
+#if WITH_EDITORONLY_DATA
+	GetSpriteComponent()->SetHiddenInGame(true);
+#endif
+	
 	ForwardArrow = CreateDefaultSubobject<UArrowComponent>(TEXT("ForwardDirection"));
 	ForwardArrow->SetupAttachment(GetRootComponent());
 	ForwardArrow->SetArrowFColor(FColor(0, 255, 0));
+	ForwardArrow->SetHiddenInGame(false);
 	
 	BackwardArrow = CreateDefaultSubobject<UArrowComponent>(TEXT("BackwardDirection"));
 	BackwardArrow->SetupAttachment(GetRootComponent());
 	BackwardArrow->SetArrowFColor(FColor(255, 0, 0));
 	BackwardArrow->SetRelativeRotation(FRotator(0.0f, 180.0f, 0.0f));
+	BackwardArrow->SetHiddenInGame(false);
 	
 	GetCollisionComponent()->OnComponentBeginOverlap.AddDynamic(this, &AHaunticaCameraTriggerBox::OnPlayerEntered);
 	GetCollisionComponent()->OnComponentEndOverlap.AddDynamic(this, &AHaunticaCameraTriggerBox::OnPlayerExited);
@@ -75,6 +91,11 @@ void AHaunticaCameraTriggerBox::BeginPlay()
 	
 	check(PlayerController);
 	PlayerPawn = PlayerController->GetPawn();
+	
+#if WITH_EDITOR
+	const bool bVisible = CVarShowCameraTriggers.GetValueOnGameThread();
+	SetActorHiddenInGame(!bVisible);
+#endif
 }
 
 void AHaunticaCameraTriggerBox::OnPlayerEntered(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)

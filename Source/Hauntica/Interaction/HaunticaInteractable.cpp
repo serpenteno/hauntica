89b599046e1335cc09783ee29e994a463ec4556b
Kamil Fiącek <fiacek.kamil@gmail.com>
2026-08-12 18:11:15 +0200

    feat: add interactable interface

---
+++ Source/Hauntica/Interaction/HaunticaInteractable.cpp
@@ -0,0 +1,9 @@
+// Copyright 2026 Kamil Fiącek, Wiktoria Boćko, Konrad Pietrzak (RozrywkaFan), Wiktor Kaźmierczak (hmbtoro), Antonina Gudyma (antonya). All Rights Reserved.
+
+
+#include "HaunticaInteractable.h"
+
+void IHaunticaInteractable::Interact(AHaunticaPlayerCharacter* const Interactor)
+{
+	
+}
---
+++ Source/Hauntica/Interaction/HaunticaInteractable.h
@@ -0,0 +1,23 @@
+// Copyright 2026 Kamil Fiącek, Wiktoria Boćko, Konrad Pietrzak (RozrywkaFan), Wiktor Kaźmierczak (hmbtoro), Antonina Gudyma (antonya). All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "HaunticaInteractable.generated.h"
+
+class AHaunticaPlayerCharacter;
+
+UINTERFACE()
+class UHaunticaInteractable : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class HAUNTICA_API IHaunticaInteractable
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Interact(AHaunticaPlayerCharacter* const Interactor);
+};
